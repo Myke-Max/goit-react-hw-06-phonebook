e974dbba653f410f8c562ba8014e2a713d31eb44
Mike-Max <scartman1992@gmail.com>
2021-10-16 19:22:36 +0100

    more clean

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import React, { useState, useEffect } from 'react';
 import Phonebook from './components/phonebook';
 import ContactsList from './components/contactList/ContactList';
 import Filter from './components/filterContacts';
 import { ToastContainer } from 'react-toastify';
 import Container from './components/container';
-import { v4 as uuidv4 } from 'uuid';
 
-import Counter from './components/Counter';
+// import Counter from './components/Counter';
 
 export default function App() {
   return (
     <>
       <ToastContainer />;
       <Container>
-        <Counter />
+        {/* training with Counter */}
+        {/* <Counter /> */}
         <Phonebook />
         <Filter />
         <ContactsList />
